b62b9bb5188c7751a5f5bc24e22e9bc975d9f9b7
Andrew Cui <andycui97@Andrews-MacBook-Pro.local>
2017-09-26 19:44:13 -0700

    simple_aboidance_agent v1

--- gym_urbandriving/agents/__init__.py
+++ gym_urbandriving/agents/__init__.py
@@ -1,2 +1,3 @@
 from gym_urbandriving.agents.background_agent import BackgroundAgent
 from gym_urbandriving.agents.keyboard_agent import KeyboardAgent
+from gym_urbandriving.agents.simple_avoidance_agent import SimpleAvoidanceAgent
