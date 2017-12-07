b4b3e2b95cfbf99b8ae8c7a7ad6df6f2e0d10159
Andrew Cui <andycui97@gmail.com>
2017-12-07 13:51:15 -0800

    add planning quanta for faster planning

--- gym_urbandriving/agents/accel_agent.py
+++ gym_urbandriving/agents/accel_agent.py
@@ -1,6 +1,5 @@
 from copy import deepcopy
 import numpy as np
-import time
 
 class AccelAgent:
     """
@@ -21,6 +20,7 @@ class AccelAgent:
     def __init__(self, agent_num=0):
         self.agent_num = agent_num
         self.valid_actions = [(0, 1), (3, .5), (-3, .5), (0, -1), (0, 0)]
+        self.quantum = 2
         return
 
     def eval_policy(self, state, nsteps=8):
@@ -56,12 +56,14 @@ class AccelAgent:
 
             angle_offset = abs((state_copy.dynamic_objects[self.agent_num].angle-45)%90-45)
 
-            for i in range(nsteps):
-                for i, dobj in enumerate(state_copy.dynamic_objects):
-                    if i != self.agent_num:
-                        dobj.step((0, 0))
-                    else:
-                        dobj.step(action)
+            for z in range(nsteps//self.quantum):
+                for y in range(self.quantum):
+                    for i, dobj in enumerate(state_copy.dynamic_objects):
+                        if i != self.agent_num:
+                            dobj.step((0, 0))
+                        else:
+                            dobj.step(action)
+
                 time += 1
                 if (state_copy.collides_any(self.agent_num)):
                     break
@@ -70,4 +72,5 @@ class AccelAgent:
                 best_action = action
                 best_time = time
                 best_angle_offset = angle_offset
+  
         return best_action
