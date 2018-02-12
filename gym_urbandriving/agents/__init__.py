8cda4329ecb6ae77ceeb1ca31c33e8af3c1a2737
Andrew Cui <andycui97@gmail.com>
2018-02-12 12:46:06 -0800

    fix imports again

--- gym_urbandriving/agents/__init__.py
+++ gym_urbandriving/agents/__init__.py
@@ -2,6 +2,5 @@ from gym_urbandriving.agents.keyboard_agent import KeyboardAgent
 from gym_urbandriving.agents.accel_agent import AccelAgent
 from gym_urbandriving.agents.null_agent import NullAgent
 from gym_urbandriving.agents.traffic_light_agent import TrafficLightAgent
-from gym_urbandriving.agents.tree_search_agent import TreeSearchAgent
 from gym_urbandriving.agents.pursuit_agent import PursuitAgent
 from gym_urbandriving.agents.control_agent import ControlAgent
