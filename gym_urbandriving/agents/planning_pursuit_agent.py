f356813f1e4e21d60de1f87c91cc8b4fdbbb828c
Andrew Cui <andycui97@gmail.com>
2018-03-06 16:59:37 -0800

    velocity planning to seperate planner

--- gym_urbandriving/agents/planning_pursuit_agent.py
+++ gym_urbandriving/agents/planning_pursuit_agent.py
@@ -1,8 +1,7 @@
 import numpy as np
 from gym_urbandriving.utils.PID import PIDController
-from gym_urbandriving.agents.pursuit_agent import PursuitAgent
-from gym_urbandriving.assets import Car, TrafficLight
-from gym_urbandriving.agents import NullAgent, TrafficLightAgent, PursuitAgent
+from gym_urbandriving.agents import PursuitAgent
+from gym_urbandriving.planning import VelocityMPCPlanner
 from copy import deepcopy
 import gym_urbandriving as uds
 
@@ -32,51 +31,16 @@ class PlanningPursuitAgent(PursuitAgent):
         action
             Keyboard action
         """
-        agents = []
-        for i,obj in enumerate(state.dynamic_objects):
-            if type(obj) in {Car}:
-                agents.append(PursuitAgent(i))
-            elif type(obj) in {TrafficLight}:
-                agents.append(TrafficLightAgent(i))
-            else:
-                agents.append(NullAgent(i))
 
-        state_copy = deepcopy(state)
-        testing_env = uds.UrbanDrivingEnv(init_state=state_copy,
-                                  visualizer=None,
-                                  max_time=500,
-                                  randomize=False,
-                                  agent_mappings={Car:NullAgent,
-                                                  TrafficLight:TrafficLightAgent},
-                                  use_ray=False
-        )
-        state_copy = testing_env.current_state
-        if state_copy.dynamic_objects[self.agent_num].trajectory.stopped:
-            state_copy.dynamic_objects[self.agent_num].trajectory.restart()
-            for t in range(10):
-                actions = []
-                for agent in agents:
-                    action = agent.eval_policy(state_copy)
-                    actions.append(action)
-                state_copy, reward, done, info_dict = testing_env._step_test(actions)
-                done = state_copy.collides_any_dynamic(self.agent_num)
-                if done:
-                    break
-            if not done:
-                state.dynamic_objects[self.agent_num].trajectory.restart()
 
-        elif not state_copy.dynamic_objects[self.agent_num].trajectory.stopped:
-            for t in range(10):
-                actions = []
-                for agent in agents:
-                    action = agent.eval_policy(state_copy)
-                    actions.append(action)
-                state_copy, reward, done, info_dict = testing_env._step_test(actions)
-                done = state_copy.collides_any_dynamic(self.agent_num)
-                if done:
-                    break
-            if done:
-                state.dynamic_objects[self.agent_num].trajectory.modify_to_stop()
+
+        target_vel = VelocityMPCPlanner().plan(deepcopy(state), self.agent_num)
+        if target_vel == 4:
+            state.dynamic_objects[self.agent_num].trajectory.restart()
+        elif target_vel == 0:
+            state.dynamic_objects[self.agent_num].trajectory.modify_to_stop()
+        else:
+            pass
 
         return super(PlanningPursuitAgent, self).eval_policy(state)
 
