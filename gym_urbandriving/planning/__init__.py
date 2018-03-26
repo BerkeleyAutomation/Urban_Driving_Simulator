af0c3c7b199a26eb6effece337fd4f35cc8d6e1b
Michael Laskey <mdlaskey@umich.edu>
2018-03-26 09:56:15 -0700

    fixed indent error in travis test

--- gym_urbandriving/planning/__init__.py
+++ gym_urbandriving/planning/__init__.py
@@ -3,8 +3,8 @@ from gym_urbandriving.planning.vel_mpc_planner import VelocityMPCPlanner
 
 
 try:
-    from gym_urbandriving.planning.rrt_multi_planner import RRTMPlanner
-    from gym_urbandriving.planning.geometric_planner import GeometricPlanner
+	from gym_urbandriving.planning.rrt_multi_planner import RRTMPlanner
+	from gym_urbandriving.planning.geometric_planner import GeometricPlanner
 except ImportError:
 	from gym_urbandriving.planning.geometric_planner_lite import GeometricPlanner
-    print("OMPL not supported. RRT unavailable")
+	print("OMPL not supported. RRT unavailable")
