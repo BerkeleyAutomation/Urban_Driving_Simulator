485ec3b0f27a9a7d6f2760c77af3067249d529d3
Michael Laskey <mdlaskey@umich.edu>
2018-03-26 09:53:43 -0700

    added a non-ompl planner to pass travis

--- gym_urbandriving/planning/__init__.py
+++ gym_urbandriving/planning/__init__.py
@@ -1,9 +1,10 @@
 from gym_urbandriving.planning.trajectory import Trajectory
-from gym_urbandriving.planning.casteljau_planner import CasteljauPlanner
 from gym_urbandriving.planning.vel_mpc_planner import VelocityMPCPlanner
+
 
 try:
     from gym_urbandriving.planning.rrt_multi_planner import RRTMPlanner
     from gym_urbandriving.planning.geometric_planner import GeometricPlanner
 except ImportError:
+	from gym_urbandriving.planning.geometric_planner_lite import GeometricPlanner
     print("OMPL not supported. RRT unavailable")
