bbb9076e9927fda669c85c16869087c82a93d26f
Hankun Zhao <jerryz123@berkeley.edu>
2018-04-22 18:52:49 -0700

    Added png jpg and json to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gym_urbandriving',
-      version='0.0.2',
+      version='0.0.3',
       install_requires=["gym<=0.9.5",
                         "image",
                         "scipy",
