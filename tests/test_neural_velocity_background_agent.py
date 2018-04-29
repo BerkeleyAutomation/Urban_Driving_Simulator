6dccf4368a828cc439b79a3d133282e5612efee0
Michael Laskey <mdlaskey@umich.edu>
2018-04-29 10:15:57 -0700

    added neural supervisor and background agent tests

---
+++ tests/test_neural_velocity_background_agent.py
@@ -0,0 +1,29 @@
+import json
+import gym
+import gym_urbandriving as uds
+from gym_urbandriving import *
+from gym_urbandriving.agents import *
+from gym_urbandriving.assets import *
+from gym_urbandriving.utils import Trajectory
+import numpy as np
+
+with open('configs/default_config.json') as json_data_file:
+    data = json.load(json_data_file)
+
+
+data['agents']['action_space'] = "velocity"
+data['recorded_data']['state_space'] = 'raw'
+data['agents']['action_space'] = "steering"
+data['agents']['agent_mappings']['Car'] = "NeuralPursuitAgent"
+
+
+sup = SteeringSupervisor(agent_num = 0)
+
+env = uds.UrbanDrivingEnv(data)
+state = env.current_state
+
+for i in range(3): 
+    action = sup.eval_policy(state)
+    obs,reward,done,info_dict = env.step([action])
+    state = obs[0]
+
