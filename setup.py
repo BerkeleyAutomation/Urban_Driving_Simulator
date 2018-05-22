87aa12f4cc8d95ed1841da13311d946da7539a0d
Jerry Zhao <jerryz123@berkeley.edu>
2018-05-21 22:24:30 -0700

    Version bump for FLUIDS_IL

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gym_urbandriving',
-      version='0.0.3',
+      version='0.0.4',
       install_requires=["gym<=0.9.5",
                         "image",
                         "scipy",
