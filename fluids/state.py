35a8f1653b04a64147bcf3c883941b67bb2d77f1
Jerry Zhao <jerryz123@berkeley.edu>
2018-07-14 16:19:51 -0700

    Cached waypoints

--- fluids/state.py
+++ fluids/state.py
@@ -4,10 +4,13 @@ import os
 from six import iteritems
 import random
 import pygame
+import hashlib
 
 from fluids.consts import *
 from fluids.assets import *
 from fluids.utils import *
+from fluids.version import __version__
+
 
 basedir = os.path.dirname(__file__)
 
@@ -23,13 +26,22 @@ class State(object):
     def __init__(self,
                  layout          =STATE_CITY,
                  controlled_cars =0,
-                 control_space   =CTRL_STEERING,
                  background_cars =0,
                  background_peds =0,
                  vis_level       =1):
 
         fluids_print("Loading layout: " + layout)
-        layout = json.load(open(os.path.join(basedir, "layouts", layout + ".json")))
+        cfilename = layout + __version__ + ".json"
+        layout = open(os.path.join(basedir, "layouts", layout + ".json"))
+        cached_layout = lookup_cache(cfilename)
+        cache_found = cached_layout is not False
+        if cached_layout:
+            fluids_print("Cached layout found")
+            layout = cached_layout
+
+        layout = json.load(layout)
+
+        
         self.time             = 0
         self.objects          = {}
         self.type_map         = {k:{} for k in [Terrain, Lane, Street, CrossWalk, Sidewalk,
@@ -49,7 +61,7 @@ class State(object):
                    "Street"     : Street,
                    "CrossWalk"  : CrossWalk,
                    "PedCrossing": PedCrossing,
-                   "Sidewalk"   : Sidewalk}[obj_info.pop('type')]
+                   "Sidewalk"   : Sidewalk}[obj_info['type']]
 
             obj = typ(state=self, vis_level=vis_level, **obj_info)
 
@@ -67,7 +79,7 @@ class State(object):
             typ = {"Car"           : Car,
                    "TrafficLight"  : TrafficLight,
                    "CrossWalkLight": CrossWalkLight,
-                   "Pedestrian"    : Pedestrian}[obj_info.pop('type')]
+                   "Pedestrian"    : Pedestrian}[obj_info['type']]
 
             obj = typ(state=self, vis_level=vis_level, **obj_info)
 
@@ -77,6 +89,43 @@ class State(object):
             self.type_map[typ][key] = obj
             self.objects[key] = obj
             self.dynamic_objects[key] = obj
+
+
+        fluids_print("Generating trajectory map")
+        if 'waypoints' in layout:
+            wp_map = {}
+            self.waypoints = []
+            self.ped_waypoints = []
+
+            for wp_info in layout['waypoints']:
+                index = wp_info.pop('index')
+                wp = Waypoint(**wp_info)
+                wp_map[index] = wp
+                self.waypoints.append(wp)
+            for wp in self.waypoints:
+                wp.nxt = [wp_map[index] for index in wp.nxt]
+
+            wp_map = {}
+            for wp_info in layout['ped_waypoints']:
+                index = wp_info.pop('index')
+                wp = Waypoint(**wp_info)
+                wp_map[index] = wp
+                self.ped_waypoints.append(wp)
+            for wp in self.ped_waypoints:
+                wp.nxt = [wp_map[index] for index in wp.nxt]
+        else:
+            self.generate_waypoints_init()
+        
+            layout['waypoints'] = []
+            layout['ped_waypoints'] = []
+            for wp in self.waypoints:
+                wpdict = {"index":wp.index, "x":wp.x, "y": wp.y, "angle":wp.angle,
+                          "nxt":[w.index for w in wp.nxt]}
+                layout['waypoints'].append(wpdict)
+            for wp in self.ped_waypoints:
+                wpdict = {"index":wp.index, "x":wp.x, "y": wp.y, "angle":wp.angle,
+                          "nxt":[w.index for w in wp.nxt]}
+                layout['ped_waypoints'].append(wpdict)
 
         fluids_print("Generating cars")
         for i in range(controlled_cars + background_cars):
@@ -89,20 +138,64 @@ class State(object):
                 min_d = min([car.dist_to(other) for k, other in iteritems(self.type_map[Car])] + [np.inf])
                 if min_d > 10 and not self.is_in_collision(car):
                     key = get_id()
+                    for waypoint in self.waypoints:
+                        if car.intersects(waypoint):
+                            while car.intersects(waypoint):
+                                waypoint = random.choice(waypoint.nxt)
+                            waypoint = random.choice(waypoint.nxt)
+                            car.waypoints = [waypoint]
+                            break
                     self.type_map[Car][key] = car
                     self.objects[key] = car
                     car_ids.append(key)
                     self.dynamic_objects[key] = car
                     break
 
-
-
         self.controlled_cars = {k: self.objects[k] for k in car_ids[:controlled_cars]}
         for k, car in iteritems(self.controlled_cars):
             car.color = (155, 20, 20)
         self.background_cars = {k: self.objects[k] for k in car_ids[controlled_cars:]}
 
-        fluids_print("Generating trajectory map")
+
+        fluids_print("Generating peds")
+        for i in range(background_peds):
+            while True:
+                wp = random.choice(self.ped_waypoints)
+                ped = Pedestrian(state=self, x=wp.x, y=wp.y, angle=wp.angle, vis_level=vis_level)
+                while ped.intersects(wp):
+                    wp = random.choice(wp.nxt)
+                ped.waypoints = [wp]
+                if not self.is_in_collision(ped):
+                    key = get_id()
+                    self.objects[key] = ped
+                    self.type_map[Pedestrian][key] = ped
+                    self.dynamic_objects[key] = obj
+                    break
+
+
+        if not cache_found:
+            fluids_print("Caching layout to: " + cfilename)
+            with open(get_cache_filename(cfilename), "w") as outfile:
+                json.dump(layout, outfile, indent=1)
+        fluids_print("State creation complete")
+        if vis_level:
+            self.static_surface       = pygame.Surface(self.dimensions)
+            try:
+                self.static_debug_surface = pygame.Surface(self.dimensions, pygame.SRCALPHA)
+            except ValueError:
+                fluids_print("WARNING: Alpha channel not available. Visualization may be slow")
+                self.static_debug_surface = self.static_surface.copy()
+            for k, obj in iteritems(self.static_objects):
+                if type(obj) != CrossWalk:
+                    obj.render(self.static_surface)
+                else:
+                    obj.render(self.static_debug_surface)
+            for waypoint in self.waypoints:
+                waypoint.render(self.static_debug_surface)
+            for waypoint in self.ped_waypoints:
+                waypoint.render(self.static_debug_surface, color=(255, 255, 0))
+
+    def generate_waypoints_init(self):
         self.waypoints      = [lane.start_waypoint for k, lane in iteritems(self.type_map[Lane])]
         self.waypoints.extend([lane.end_waypoint   for k, lane in iteritems(self.type_map[Lane])])
 
@@ -125,14 +218,6 @@ class State(object):
         for waypoint in self.waypoints:
             new_waypoints.extend(waypoint.smoothen(smooth_level=2000))
         self.waypoints.extend(new_waypoints)
-        for k, car in iteritems(self.type_map[Car]):
-            for waypoint in self.waypoints:
-                if car.intersects(waypoint):
-                    while car.intersects(waypoint):
-                        waypoint = random.choice(waypoint.nxt)
-                    waypoint = random.choice(waypoint.nxt)
-                    car.waypoints = [waypoint]
-                    break
 
         self.ped_waypoints = []
         for k, obj in iteritems(self.objects):
@@ -159,39 +244,13 @@ class State(object):
             new_waypoints.extend(waypoint.smoothen(smooth_level=1500))
         self.ped_waypoints.extend(new_waypoints)
 
-        fluids_print("Generating peds")
-        for i in range(background_peds):
-            while True:
-                wp = random.choice(self.ped_waypoints)
-                ped = Pedestrian(state=self, x=wp.x, y=wp.y, angle=wp.angle, vis_level=vis_level)
-                while ped.intersects(wp):
-                    wp = random.choice(wp.nxt)
-                ped.waypoints = [wp]
-                if not self.is_in_collision(ped):
-                    key = get_id()
-                    self.objects[key] = ped
-                    self.type_map[Pedestrian][key] = ped
-                    self.dynamic_objects[key] = obj
-                    break
-
-        fluids_print("State creation complete")
-        if vis_level:
-            self.static_surface       = pygame.Surface(self.dimensions)
-            try:
-                self.static_debug_surface = pygame.Surface(self.dimensions, pygame.SRCALPHA)
-            except ValueError:
-                fluids_print("WARNING: Alpha channel not available. Visualization may be slow")
-                self.static_debug_surface = self.static_surface.copy()
-            for k, obj in iteritems(self.static_objects):
-                if type(obj) != CrossWalk:
-                    obj.render(self.static_surface)
-                else:
-                    obj.render(self.static_debug_surface)
-            for waypoint in self.waypoints:
-                waypoint.render(self.static_debug_surface)
-            for waypoint in self.ped_waypoints:
-                waypoint.render(self.static_debug_surface, color=(255, 255, 0))
-
+        i = 0
+        for wp in self.waypoints:
+            wp.index = i
+            i += 1
+        for wp in self.ped_waypoints:
+            wp.index = i
+            i += 1
 
     def get_static_surface(self):
         return self.static_surface
--- fluids/utils/__init__.py
+++ fluids/utils/__init__.py
@@ -1,7 +1,4 @@
 from fluids.utils.utils import *
-
 from fluids.utils.debug import *
 from fluids.utils.rewards import path_reward
-
-
 from fluids.utils.pid import PIDController
