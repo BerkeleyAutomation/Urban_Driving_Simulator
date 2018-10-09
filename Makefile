26f6c81fb6f63b5d1a72503c3395a824c5ada491
Jerry Zhao <jerryz123@berkeley.edu>
2018-10-09 13:38:25 -0700

    Re-enable q-lidar

--- Makefile
+++ Makefile
@@ -7,9 +7,11 @@ test: clean
 	$(PY) -m fluids --time 100 -v 0 -o birdseye --datasaver="~/data/fluids_data"
 	$(PY) -m fluids --time 100 -v 0 -o grid --datasaver="~/data/fluids_data"
 	$(PY) -m fluids --time 100 -v 0 -o none --datasaver="~/data/fluids_data"
+	$(PY) -m fluids --time 100 -v 0 -o qlidar --datasaver="~/data/fluids_data"
 	$(GUI) $(PY) -m fluids --time 100 -v 99 -o birdseye --datasaver="~/data/fluids_data"
 	$(GUI) $(PY) -m fluids --time 100 -v 99 -o grid --datasaver="~/data/fluids_data"
 	$(GUI) $(PY) -m fluids --time 100 -v 99 -o none --datasaver="~/data/fluids_data"
+	$(GUI) $(PY) -m fluids --time 100 -v 0 -o qlidar --datasaver="~/data/fluids_data"
 	$(GUI) $(PY) tests/test_gym.py 
 	$(GUI) $(PY) tests/test_gym_supervisor.py
 	$(GUI) $(PY) tests/test_grid_obs.py
@@ -17,9 +19,11 @@ coverage: clean
 	$(COV) -m fluids --time 100 -v 0 -o birdseye --datasaver="~/data/fluids_data"
 	$(COV) -m fluids --time 100 -v 0 -o grid --datasaver="~/data/fluids_data"
 	$(COV) -m fluids --time 100 -v 0 -o none --datasaver="~/data/fluids_data"
+	$(COV) -m fluids --time 100 -v 0 -o qlidar --datasaver="~/data/fluids_data"
 	$(GUI) $(COV) -m fluids --time 100 -v 99 -o birdseye --datasaver="~/data/fluids_data"
 	$(GUI) $(COV) -m fluids --time 100 -v 99 -o grid --datasaver="~/data/fluids_data"
 	$(GUI) $(COV) -m fluids --time 100 -v 99 -o none --datasaver="~/data/fluids_data"
+	$(GUI) $(COV) -m fluids --time 100 -v 99 -o qlidar --datasaver="~/data/fluids_data"
 	$(GUI) $(COV) tests/test_gym.py
 	$(GUI) $(COV) tests/test_gym_supervisor.py
 	$(GUI) $(COV) tests/test_grid_obs.py
--- fluids/obs/qlidar.py
+++ fluids/obs/qlidar.py
@@ -1,2 +1,78 @@
-def qlidar_observer(key, state):
-    raise NotImplementedError
+from six import iteritems
+import numpy as np
+import pygame
+import shapely
+
+from fluids.obs.obs import FluidsObs
+from fluids.utils import distance
+
+class QLidarObservation(FluidsObs):
+    def __init__(self, car, det_range=200, n_beams=8):
+        from fluids.assets import Shape
+
+        state = car.state
+
+        self.car = car
+        self.det_range = det_range
+
+        self.grid_square = Shape(x=car.x, y=car.y,
+                                 xdim=det_range*2, ydim=det_range*2,
+                                 angle=car.angle,
+                                 color=None)
+
+        self.all_collideables = []
+
+        for c in self.car.collideables:
+            for k, obj in iteritems(state.type_map[c]):
+                if car.can_collide(obj) and self.grid_square.intersects(obj):
+                    self.all_collideables.append(obj)
+
+        x, y = car.x, car.y
+        beam_deltas = (np.arange(n_beams)/float(n_beams/2)-1)*np.pi
+        self.linestrings = []
+        self.detections = []
+        for beam_delta in beam_deltas:
+            beam_angle = car.angle + beam_delta
+
+            xd = x + np.cos(beam_angle) * det_range
+            yd = y - np.sin(beam_angle) * det_range
+            linestring = shapely.geometry.LineString([(x, y), (xd, yd)])
+            self.linestrings.append((beam_delta, linestring))
+
+            min_coll_d = det_range
+            for obj in self.all_collideables:
+                isect = linestring.intersection(obj.shapely_obj)
+                if not isect.is_empty:
+                    d = distance((x, y), list(isect.coords)[0])
+                    if d < min_coll_d:
+                        min_coll_d = d
+            self.detections.append([min_coll_d])
+
+    def get_array(self):
+        return np.array(self.detections)
+            
+    def render(self, surface):
+        self.grid_square.render(surface, border=10)
+        if self.car.vis_level > 4:
+            for obj in self.all_collideables:
+                obj.render_debug(surface)
+            for det, (beam_delta, ls) in \
+                zip(self.detections, self.linestrings):
+
+                # pygame.draw.line(surface, (0, 255, 0),
+                #                  (self.car.x, self.car.y),
+                #                  ls.coords[1],
+                #                  5)
+                angle = beam_delta + self.car.angle
+                xd = np.cos(angle) * det[0]
+                yd = np.sin(angle) * det[0]
+
+                color = (255, 0, 0)
+                if det[0] == self.det_range:
+                    color = (0, 255, 0)
+                pygame.draw.line(surface,
+                                 color,
+                                 (self.car.x, self.car.y),
+                                 (self.car.x+xd, self.car.y-yd),
+                                 4)
+                                 
