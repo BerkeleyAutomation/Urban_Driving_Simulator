b5d84d50a016e200d80516e5c2bc46358da43991
raghavanand98 <raghav_anand98@yahoo.com>
2018-10-10 19:47:43 -0700

    modified grid observation description

--- fluids/obs/grid.py
+++ fluids/obs/grid.py
@@ -11,9 +11,9 @@ class GridObservation(FluidsObs):
     """
     Grid observation type. 
     Observation is an occupancy grid over the detection region. 
-    Observation has 9 dimensions: terrain, drivable regions, illegal drivable 
-    regions, cars, pedestrians, traffic lights, way points, point trajectory and edge trajectory.
-    Array representation is (grid_size, grid_size, 9)
+    Observation has 11 dimensions: terrain, drivable regions, illegal drivable 
+    regions, cars, pedestrians, traffic lights x 3, way points, point trajectory and edge trajectory.
+    Array representation is (grid_size, grid_size, 11)
     """
     def __init__(self, car, obs_dim=500, shape=(500,500)):
         from fluids.assets import ALL_OBJS, TrafficLight, Lane, Terrain, Sidewalk, \
