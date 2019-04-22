94515134f155798acc0e749bb902852d372294de
amandhar <amandhar@berkeley.edu>
2019-04-21 21:43:38 -0700

    Added distance from waypoint to car to WaypointVelAction

--- fluids/actions.py
+++ fluids/actions.py
@@ -35,7 +35,7 @@ class WaypointVelAction(Action):
 
     Parameters
     ----------
-    waypoint: tuple of (x, y, angle)
+    waypoint: tuple of (x, y, angle, distance) all relative to car
     vel: float in range (0, 1)
     """
     def __init__(self, waypoint, vel):
@@ -44,7 +44,7 @@ class WaypointVelAction(Action):
     def get_action(self):
         return self.waypoint, self.vel
     def get_array(self):
-        return np.array([self.waypoint[0], self.waypoint[1], self.waypoint[2], self.vel])
+        return np.array([self.waypoint[0], self.waypoint[1], self.waypoint[2], self.waypoint[3], self.vel])
 
 class SteeringAccAction(Action):
     """
